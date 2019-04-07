818276d63132d591b59eb59c901a0f0cf455a3d7
Miguel Ramos <miguel.marques.ramos@gmail.com>
2019-04-07 17:57:52 +0100

    feat(between): between function

--- libs/support/src/lib/support.spec.ts
+++ libs/support/src/lib/support.spec.ts
@@ -1,10 +1,17 @@
-import { uniqueID } from "./support";
+import { uniqueID, between } from "./support";
 
-describe('> Support helper functions', () => {
-  it('# Should create a valid UUID V4', () => {
+describe('> Support helper functions.', () => {
+  it('# Should create a valid UUID V4.', () => {
     const regex = /^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i;
     const uuid = uniqueID();
 
     expect(regex.test(uuid)).toBeTruthy();
+  });
+
+  it('# Should check if number is between an interval.', () => {
+    expect(between(5, 2, 7, false)).toBeTruthy();
+    expect(between(1, 2, 4, false)).toBeFalsy();
+
+    expect(between(3, 1, 3, true)).toBeTruthy();
   });
 });
--- libs/support/src/lib/support.ts
+++ libs/support/src/lib/support.ts
@@ -1,4 +1,12 @@
 /**
+* @license
+* Copyright NgLab All Rights Reserved.
+*
+* Use of this source code is governed by an MIT-style license that can be
+* found in the LICENSE file at https://www.ng-lab.com/license
+*/
+
+/**
  * Create unique UUID
  *
  * @export
@@ -10,4 +18,28 @@ export function uniqueID(): string {
     const r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
     return v.toString(16);
   });
+}
+
+/**
+ * Function to check if a number is between interval
+ *
+ * @export
+ * @param {number} num
+ * @param {number} min
+ * @param {number} max
+ * @param {boolean} [inclusive=true]
+ * @returns {boolean}
+ */
+export function between(
+  num: number,
+  min: number,
+  max: number,
+  inclusive = true
+): boolean {
+  const minValue = Math.min.apply(Math, [min, max]);
+  const maxValue = Math.max.apply(Math, [min, max]);
+
+  return inclusive
+    ? num >= minValue && num <= maxValue
+    : num > minValue && num < maxValue;
 }
