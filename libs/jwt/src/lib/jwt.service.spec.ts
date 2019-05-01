242eb70ae89bca6dc52602c6bdaea8e7d6e4e3f9
Miguel Ramos <miguel.marques.ramos@gmail.com>
2019-05-01 19:08:03 +0100

    test(jwt): exceptions types

--- libs/jwt/src/lib/jwt.service.spec.ts
+++ libs/jwt/src/lib/jwt.service.spec.ts
@@ -1,8 +1,12 @@
 import { JWTService } from './jwt.service';
+import { JWTSplitDecodeError, JWTIllegalBaseDecodeError } from './jwt-errors';
 
 describe('> JWTService', () => {
   let jwtService: JWTService;
   const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJUZXN0IFRva2VuIiwiaWF0IjoxNDkzNTEyNzgxLCJleHAiOjE1MjQ5NjIzODEsImF1ZCI6Ind3dy5leGFtcGxlLmNvbSIsInN1YiI6Impyb2NrZXRAZXhhbXBsZS5jb20iLCJHaXZlbk5hbWUiOiJKb2hubnkiLCJTdXJuYW1lIjoiUm9ja2V0IiwiRW1haWwiOiJqcm9ja2V0QGV4YW1wbGUuY29tIiwiUm9sZSI6WyJNYW5hZ2VyIiwiUHJvamVjdCBBZG1pbmlzdHJhdG9yIl19.SvbfH5galrs6gbM6hcB4zdhBTK7rW1-ZCS90wMmtDTE";
+  const tokenInvalid = "bGciOiJIUzI1NiJ9.eyJpc3MiOiJUZXN0ImF1ZCI6Ind3dy5leGFtcGxlLmNvbSIsInN1YiI6Impyb2NrZXRAZXhhbXBsZS5jb20iLCJHaXZlbk5hbWUiOiJKb2hubnkiLCJTdXJuYW1lIjoiUm9ja2V0IiwiRW1haWwiOiJqcm9ja2V0QGV4YW1wbGUuY29tIIl19.SvbfH5galrs6gbM6hcB4zdhBTK7rW1-ZCS90wMm";
+  const tokenIncomplete = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJUZXN0IFRva2VuIiwiaWF0IjoxNDkzNTEyNzgxLCJleHAiOjE1MjQ5NjIzODEsImF1ZCI6Ind3dy5leGFtcGxlLmNvbSIsInN1YiI6Impyb2NrZXRAZXhhbXBsZS5jb20iLCJHaXZlbk5hbWUiOiJKb2hubnkiLCJTdXJuYW1lIjoiUm9ja2V0IiwiRW1haWwiOiJqcm9ja2V0QGV4YW1wbGUuY29tIiwiUm9sZSI6WyJNYW5hZ2VyIiwiUHJvamVjdCBBZG1pbmlzdHJhdG9yIl19";
+
   beforeEach(() => {
     jwtService = new JWTService();
   });
@@ -27,5 +31,13 @@ describe('> JWTService', () => {
     const isExpired = jwtService.isTokenExpired(token);
 
     expect(isExpired).toBeTruthy();
+  });
+
+  it('# Should throw JWTSplitDecodeError', () => {
+    expect(() => { jwtService.decodeToken(tokenIncomplete); }).toThrow(JWTSplitDecodeError);
+  });
+
+  it('# Should throw JWTIllegalBaseDecodeError', () => {
+    expect(() => { jwtService.decodeToken(tokenInvalid); }).toThrow(JWTIllegalBaseDecodeError);
   });
 });
