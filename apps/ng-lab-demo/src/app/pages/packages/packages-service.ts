7913b886b7ba072bdce6ba9683421e0b76fe7bef
Miguel Ramos <miguel.marques.ramos@gmail.com>
2020-03-19 17:54:44 +0000

    fix(demo): cli prod option (#34)

--- apps/ng-lab-demo/src/app/pages/packages/packages-service.ts
+++ apps/ng-lab-demo/src/app/pages/packages/packages-service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpUrlService } from '@ng-lab/http/url';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { map, mergeMap, toArray } from 'rxjs/operators';
 import { PackageModel } from './state/packages-model';
+import { from } from 'rxjs';
+import { environment } from '@ng-lab/app/environments/environment';
 
 @Injectable()
 export class PackagesService {
@@ -15,7 +17,17 @@ export class PackagesService {
     const urlPackages = this.urlService.get('PACKAGES.FETCH');
 
     return this.httpService.get<{data: PackageModel[]}>(urlPackages).pipe(
-      map(response => response.data)
+      //map(response => response.data),
+      mergeMap(response => from(response.data).pipe(
+        map(pkg => {
+          if (environment.production) {
+            pkg.md = '/ng-lab/'+pkg.md;
+          }
+
+          return pkg;
+        }),
+        toArray()
+      ))
     );
   }
 }
